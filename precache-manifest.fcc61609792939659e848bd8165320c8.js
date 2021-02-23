d2fb7f71fc8a776d7c634878ec0c357746386de6
Dhanushga Lionel <32670499+DhanushgaLionel@users.noreply.github.com>
2021-02-23 18:56:44 -0500

    Updates

---
+++ precache-manifest.fcc61609792939659e848bd8165320c8.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc94b11b0191a97a921a6950ef70e98",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "a9e357ccc1b010d349a0",
+    "url": "/react-portfolio/static/css/3.3a26ebde.chunk.css"
+  },
+  {
+    "revision": "f6969994a1d449c6db26",
+    "url": "/react-portfolio/static/css/4.ea3dbbf2.chunk.css"
+  },
+  {
+    "revision": "2350f446c181421b254d",
+    "url": "/react-portfolio/static/css/main.ac34ba18.chunk.css"
+  },
+  {
+    "revision": "0bf24361d521466d1129",
+    "url": "/react-portfolio/static/js/2.b6f73d06.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/react-portfolio/static/js/2.b6f73d06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9e357ccc1b010d349a0",
+    "url": "/react-portfolio/static/js/3.8b38c14f.chunk.js"
+  },
+  {
+    "revision": "f6969994a1d449c6db26",
+    "url": "/react-portfolio/static/js/4.55b68e65.chunk.js"
+  },
+  {
+    "revision": "2350f446c181421b254d",
+    "url": "/react-portfolio/static/js/main.28af4531.chunk.js"
+  },
+  {
+    "revision": "740cd3ce91f6370d4bf6",
+    "url": "/react-portfolio/static/js/runtime-main.ebc175ff.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/react-portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "1fcf0076e600a0d022fa75325bc07355",
+    "url": "/react-portfolio/static/media/GPUStore.1fcf0076.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/react-portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "96de6788fb67792cd2a27669a05dbaa4",
+    "url": "/react-portfolio/static/media/capstone.96de6788.jpg"
+  },
+  {
+    "revision": "06ee4201ee4fb8d5f4d8b6bf01f0f28f",
+    "url": "/react-portfolio/static/media/car4u2.06ee4201.jpg"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/react-portfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "8fc47679f7ded6975bdb5d8810452069",
+    "url": "/react-portfolio/static/media/comfyhouse.8fc47679.jfif"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/react-portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "53b2ad717fb383dc83d8ec5662e35fa7",
+    "url": "/react-portfolio/static/media/cpLogo.53b2ad71.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/react-portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/react-portfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/react-portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "1ab97c443d176ca6860b95efe194a7bb",
+    "url": "/react-portfolio/static/media/mytrack3.1ab97c44.jpg"
+  },
+  {
+    "revision": "39741664773bc71c3ec6cc8677063a04",
+    "url": "/react-portfolio/static/media/otulogo.39741664.png"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/react-portfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "eeffab6e9272689dc7e983a6353aa474",
+    "url": "/react-portfolio/static/media/react1.eeffab6e.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/react-portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "a1d744ccc42b5265d8deae4c0a6fface",
+    "url": "/react-portfolio/static/media/stan.a1d744cc.jfif"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/react-portfolio/static/media/talksCardBack.39eff4a9.svg"
+  },
+  {
+    "revision": "53015aad4cf940e4d341c556ac2d8014",
+    "url": "/react-portfolio/static/media/vodafoneLogo.53015aad.png"
+  }
+]);
